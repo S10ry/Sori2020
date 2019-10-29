665b22a87a5dfd1a52bbe7cbf00a5b174a7d2e9b
Sory <sory@MacBook-Pro-de-Sory.local>
2019-10-29 23:08:32 +0100

    index page changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,90 +1,35 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-  <div class="white-text-container background-image-container" style="background-image: url('./images/img-home.jpg')">
-    <div class="opacity"></div>
-    <div class="container">
-      <div class="row">     
-        <div class="col-md-6">
-          <h1>Election municipale de 2020</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing commodo elit at imperdiet dui accumsan sit. Ipsum dolor sit amet consectetur adipiscing elit. </p>
-          <a href="./download.html" title="" class="btn btn-lg btn-primary">En savoir plus</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="section-container border-section-container">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 section-container-spacer">
-          <div class="text-center">
-            <h2>Nos grandes axes</h2>
-            <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing.<br> Porta non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. </p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="fa-container">
-            <i class="fa fa-comment-o fa-3x" aria-hidden="true"></i>
-          </div>
-          <div class="text-center">
-            <h3>Consectetur</h3>
-          </div>
-          <div>
-            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
-              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
-            Turpis egestas pretium aenean pharetra magna ac.</p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="fa-container">
-            <i class="fa fa-heart-o fa-3x" aria-hidden="true"></i>
-          </div>
-          <div class="text-center">
-            <h3>Malesuada</h3>
-          </div>
-          <div>
-            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
-              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
-              Turpis egestas pretium aenean pharetra magna ac.
-            </p>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="fa-container">
-            <i class="fa fa-bell-o fa-3x" aria-hidden="true"></i>
-          </div>
-          <div class="text-center">
-            <h3>Phasellus</h3>
-          </div>
-          <div>
-            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
-              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
-              Turpis egestas pretium aenean pharetra magna ac. 
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="section-container background-color-container white-text-container">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="text-center">
-            <h2>Rejoindre l'equipe</h2>
-            <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta non
-              pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus. Turpis
-            egestas pretium aenean pharetra magna ac. Id cursus metus aliquam eleifend mi. </p>
-            <a href="./download.html" title="" class="btn btn-primary btn-lg">Nous contacter</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-@endsection
+        <title>Ma voix, ma ville </title>
 
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
-
-
-
+            .bg {
+            	background-image: url('/images/img-home2.jpg');
+            	height: 100%; 
+            	background-position: center;
+            	background-repeat: no-repeat;
+            	background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+    	<div class="bg" style=""></div>
+    </body>
+</html>
---
+++ resources/views/index2.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="white-text-container background-image-container" style="background-image: url('./images/img-home.jpg')">
+    <div class="opacity"></div>
+    <div class="container">
+      <div class="row">     
+        <div class="col-md-6">
+          <h1>Election municipale de 2020</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing commodo elit at imperdiet dui accumsan sit. Ipsum dolor sit amet consectetur adipiscing elit. </p>
+          <a href="./download.html" title="" class="btn btn-lg btn-primary">En savoir plus</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section-container border-section-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 section-container-spacer">
+          <div class="text-center">
+            <h2>Nos grandes axes</h2>
+            <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing.<br> Porta non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="fa-container">
+            <i class="fa fa-comment-o fa-3x" aria-hidden="true"></i>
+          </div>
+          <div class="text-center">
+            <h3>Consectetur</h3>
+          </div>
+          <div>
+            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+            Turpis egestas pretium aenean pharetra magna ac.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="fa-container">
+            <i class="fa fa-heart-o fa-3x" aria-hidden="true"></i>
+          </div>
+          <div class="text-center">
+            <h3>Malesuada</h3>
+          </div>
+          <div>
+            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+              Turpis egestas pretium aenean pharetra magna ac.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="fa-container">
+            <i class="fa fa-bell-o fa-3x" aria-hidden="true"></i>
+          </div>
+          <div class="text-center">
+            <h3>Phasellus</h3>
+          </div>
+          <div>
+            <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+              non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+              Turpis egestas pretium aenean pharetra magna ac. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section-container background-color-container white-text-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="text-center">
+            <h2>Rejoindre l'equipe</h2>
+            <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta non
+              pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus. Turpis
+            egestas pretium aenean pharetra magna ac. Id cursus metus aliquam eleifend mi. </p>
+            <a href="./download.html" title="" class="btn btn-primary btn-lg">Nous contacter</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+
+
+
+
+
